99c55547e1e08182b4b5f6daad1947dfc356b9c8
wilfrido fereira <fereira2403@gmail.com>
2023-01-26 21:19:54 -0300

    entrega

--- index.js
+++ index.js
@@ -1,182 +1,222 @@
-const btnCart = document.querySelector('.container-cart-icon');
-const containerCartProducts = document.querySelector(
-	'.container-cart-products'
-);
 
-btnCart.addEventListener('click', () => {
-	containerCartProducts.classList.toggle('hidden-cart');
-});
+//Array de productos 
+const productos = [
+    {
+        id: "remeras-01",
+        titulo: "Remera 01",
+        imagen: "./imagenes/remeras/01.jpg",
+        categoria:{
+            nombre: "remeras",
+            id: "remeras"
+        },
+        precio: 3500
+    },
+    {
+        id: "remeras-02",
+        titulo: "Remera 02",
+        imagen: "./imagenes/remeras/02.jpg",
+        categoria:{
+            nombre: "remeras",
+            id: "remeras"
+        },
+        precio: 3400
+    },  
+    {
+        id: "remeras-03",
+        titulo: "Remera 03",
+        imagen: "./imagenes/remeras/03.jpg",
+        categoria:{
+            nombre: "remeras",
+            id: "remeras"
+        },
+        precio: 3500
+    },  
+    {
+        id: "remeras-04",
+        titulo: "Remera 04",
+        imagen: "./imagenes/remeras/04.jpg",
+        categoria:{
+            nombre: "remeras",
+            id: "remeras"
+        },
+        precio: 3500
+    },    
+    {
+        id: "remeras-05",
+        titulo: "Remera 05",
+        imagen: "./imagenes/remeras/05.jpg",
+        categoria:{
+            nombre: "remeras",
+            id: "remeras"
+        },
+        precio: 3200
+    },   
+    {
+        id: "remeras-06",
+        titulo: "Remera 06",
+        imagen: "./imagenes/remeras/06.jpg",
+        categoria:{
+            nombre: "remeras",
+            id: "remeras"
+        },
+        precio: 3500
+    },
+    {
+        id: "remeras-07",
+        titulo: "Remera 07",
+        imagen: "./imagenes/remeras/07.jpg",
+        categoria:{
+            nombre: "remeras",
+            id: "remeras"
+        },
+        precio: 3000
+    },   
+    {
+        id: "remeras-08",
+        titulo: "Remera 08",
+        imagen: "./imagenes/remeras/08.jpg",
+        categoria:{
+            nombre: "remeras",
+            id: "remeras"
+        },
+        precio: 4500
+    },
+    {
+        id: "pantalon-01",
+        titulo: "Pantalon 01",
+        imagen: "./imagenes/pantalones/01.jpg",
+        categoria:{
+            nombre: "pantalones",
+            id: "pantalones"
+        },
+        precio: 5500
+    },
+    {
+        id: "pantalon-02",
+        titulo: "Pantalon 02",
+        imagen: "./imagenes/pantalones/02.jpg",
+        categoria:{
+            nombre: "pantalones",
+            id: "pantalones"
+        },
+        precio: 4500
+    },
+    {
+        id: "pantalon-03",
+        titulo: "Pantalon 03",
+        imagen: "./imagenes/pantalones/03.jpg",
+        categoria:{
+            nombre: "pantalones",
+            id: "pantalones"
+        },
+        precio: 4000
+    },{
+        id: "pantalon-04",
+        titulo: "Pantalon 04",
+        imagen: "./imagenes/pantalones/04.jpg",
+        categoria:{
+            nombre: "pantalones",
+            id: "pantalones"
+        },
+        precio: 6000
+    },
+    {
+        id: "pantalon-05",
+        titulo: "Pantalon 05",
+        imagen: "./imagenes/pantalones/05.jpg",
+        categoria:{
+            nombre: "pantalones",
+            id: "pantalones"
+        },
+        precio: 5500
+    },
+]
+//=======//
+const contenedorProductos = document.querySelector('#contenedor-productos');
+const botonesCategorias = document.querySelectorAll('.boton-categoria');
+const tituloPrincipal = document.querySelector('#titulo-principal');
+let botonesAgregar = document.querySelectorAll('.producto-agregar');
+const numerito = document.querySelector('#numerito');
 
-//==========//
-const cartinfo = document.querySelector('.cart-product')
-const rowproduct = document.querySelector('.row-product')
-const procesarCompra = document.querySelector('#procesarCompra')
-const activarFuncion = document.querySelector('#activarFuncion')
+//carga de productos
+function cargarProductos(productosElegidos) {
 
-// lista de todos los contenedores 
-const productlist = document.querySelector('.remeras_menu')
+    contenedorProductos.innerHTML = "";
 
-//Variable de arreglo de productos 
-
-
-let allproducts = JSON.parse(localStorage.getItem("allproducts")) || [];
-
-const valorTotal = document.querySelector('.total-pagar')
-
-const countProducts = document.querySelector('#contador-productos')
-
-const cartEmpty = document.querySelector('.cart-empty');
-const cartTotal = document.querySelector('.cart-total');
-
-productlist.addEventListener('click',e => {
-    
-    if(e.target.classList.contains('btn_add')){
-        const product = e.target.parentElement
-
-        const infoProduct ={
-            quantity: 1,
-            title: product.querySelector('h4').textContent,
-            price: product.querySelector('p').textContent,
-        }
-
-        const exits = allproducts.some(product => product.title === infoProduct.title)
-
-        if(exits){
-            const products = allproducts.map(product =>{
-                if(product.title === infoProduct.title){
-                    product.quantity++;
-                    return product
-                } else{
-                    return product
-                }
-            })
-            allproducts = [...products]
-
-        } else{
-            allproducts = [...allproducts, infoProduct]
-        }
-
-
-        showHTML();
-        carritoLocal();
-    }
-    
-});
-
-rowproduct.addEventListener('click', e => {
-    if(e.target.classList.contains('icon-close')){
-        const product = e.target.parentElement;
-        const title = product.querySelector('p').textContent;
-
-        allproducts = allproducts.filter(product => product.title !== title);
-
-        showHTML()
-        carritoLocal();
-    }
-})
-
-//funcion para mostrar HTML
-const showHTML = () => {
-	if (!allproducts.length) {
-		cartEmpty.classList.remove('hidden');
-		rowproduct.classList.add('hidden');
-		cartTotal.classList.add('hidden');
-	} else {
-		cartEmpty.classList.add('hidden');
-		rowproduct.classList.remove('hidden');
-		cartTotal.classList.remove('hidden');
-	}
-
-//limpiar html
-    rowproduct.innerHTML = '';
-
-    let total = 0;
-    let totalOfProducts = 0; 
-
-    allproducts.forEach(product => {
-        const containerProduct = document.createElement('div');
-        containerProduct.classList.add('cart-product');
-
-        containerProduct.innerHTML = ` 
-            <div class="info-cart-product">
-            <span class="cantidad-producto-carrito">${product.quantity}</span>
-            <p class="titulo-producto-carrito">${product.title}</p>
-            <span class="precio-producto-carrito">${product.price}</span>
-            </div>
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="icon-close"
-            >
-                <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M6 18L18 6M6 6l12 12"
-                />
-            </svg>
-    
-    `;  
-        rowproduct.append(containerProduct); 
-        
-        total = total + parseInt(product.quantity * product.price.slice(1))
-        totalOfProducts = totalOfProducts + product.quantity;
-    });
-
-    valorTotal.innerText = `$${total}`;
-    countProducts.innerText = totalOfProducts;
-};
-
-// localstorage
-
-//set item
-const carritoLocal = () =>{ 
-    localStorage.setItem("allproducts", JSON.stringify(allproducts)); 
-}
-
-//Continuacion de la compra
-procesarCompra.addEventListener('click',() => {
-    if(allproducts.length === 0){
-        Swal.fire({
-            title: "¡Tu carrito está vacio!",
-            Text: "Compra algo para continuar con la compra",
-            icon: "error",
-            confirmButtonText: "Aceptar",
-        })
-    }else{
-        location.href = "./pagina/compra.html"
-        procesarPedido();
-    }
-})
-
-//procesar compra 
-if (activarFuncion) {
-    activarFuncion.addEventListener("click", procesarPedido);
-  }
-
-function procesarPedido(){
-
-    carritoLocal.forEach((product)=>{
-        const listaCompra = document.querySelector('#lista-compra tbody')
-        const {id, nombre, precio, cantidad, img} = product;
-
-        const row = document.createElement('tr')
-        row.innerHTML += `
-        <td>
-              <img class="img-fluid img-carrito" src="${img}"/>
-              </td>
-              <td>${nombre}</td>
-            <td>${precio}</td>
-            <td>${cantidad}</td>
-            <td>${precio * cantidad}</td>
-            `;
-          listaCompra.appendChild(row);
+    productosElegidos.forEach(producto =>{
+        const div = document.createElement("div");
+        div.classList.add("producto");
+        div.innerHTML = `
+        <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+        <div class="producto-detalles">
+            <h3 class="producto-titulo">${producto.titulo}</h3>
+            <p class="producto-precio">${producto.precio}</p>
+            <button class="producto-agregar" id="${producto.id}">Agregar</button>
+        </div>  
+        `;
+        contenedorProductos.append(div);
     })
-
+    actualizarBotonesAgregar();
 }
-//llamado de las funciones 
-carritoLocal();
-showHTML();
-procesarPedido();
+cargarProductos(productos);
+
+botonesCategorias.forEach(boton => {
+    boton.addEventListener("click", (e) => {
+
+        botonesCategorias.forEach(boton => boton.classList.remove("active"));
+        e.currentTarget.classList.add("active");
+
+        if (e.currentTarget.id != "todos") {
+            const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
+            tituloPrincipal.innerText = productoCategoria.categoria.nombre;
+            const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
+            cargarProductos(productosBoton);
+        } else {
+            tituloPrincipal.innerText = "Todos los productos";
+            cargarProductos(productos);
+        }
+
+    })
+});
+// boton de agregar al carrito
+
+function actualizarBotonesAgregar(){
+    botonesAgregar = document.querySelectorAll('.producto-agregar');
+   
+    botonesAgregar.forEach(boton => {
+        boton.addEventListener("click", agregarAlCarrito);
+    });
+}
+
+let productosEnCarrito;
+
+let productosEnCarritoLS = localStorage.getItem("productos-en-carrito");
+
+if (productosEnCarritoLS) {
+    productosEnCarrito = JSON.parse(productosEnCarritoLS);
+    actualizarNumerito();
+} else {
+    productosEnCarrito = [];
+}
+
+function agregarAlCarrito(e) {
+
+    const idBoton = e.currentTarget.id;
+    const productoAgregado = productos.find(producto => producto.id === idBoton);
+
+    if(productosEnCarrito.some(producto => producto.id === idBoton)) {
+        const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+        productosEnCarrito[index].cantidad++;
+    } else {
+        productoAgregado.cantidad = 1;
+        productosEnCarrito.push(productoAgregado);
+    }
+    actualizarNumerito(); 
+
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+}
+
+function actualizarNumerito() {
+    let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    numerito.innerText = nuevoNumerito;
+}
